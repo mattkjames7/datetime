fdd1abe290e8115fa7acfd648c2d2152fdf62a7e
matt <mattkjames7@gmail.com>
2023-01-09 11:18:28 +0000

    fixed test batch file

--- test/test.bat
+++ test/test.bat
@@ -3,6 +3,6 @@ gcc -O3 -Wextra -fPIC -I..\include testc.c -o testc -lm -L. -ldatetime
 g++ -g -O3 -std=c++17 -Wextra -fPIC -I..\include testcc.cc -o testcc -lm -L. -ldatetime
 testc.exe
 testcc.exe
-del testc
-del testcc
+del testc.exe
+del testcc.exe
 del libdatetime.dll
